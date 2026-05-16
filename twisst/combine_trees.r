e9e3a2b3e0ff393df27e023e5542f00cd89d0075
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2026-05-16 00:25:51 -0500

    Add files via upload

---
+++ twisst/combine_trees.r
@@ -0,0 +1,36 @@
+# for 50 kbp directory
+#make sure in windows only RAxML_bipartitions.tre are present
+
+options(scipen=999)
+
+# list all the files in the trees directory
+x_files <- list.files("bipartitions", full.names=T)
+
+# find the chromosome, start, and end for each tree
+x_names <- list.files("bipartitions")
+x_chrom <- sapply(strsplit(sapply(strsplit(x_names, "RAxML_bipartitions."), "[[", 2), "__"), "[[", 1)
+x_start <- sapply(strsplit(x_names, "__"), "[[", 2)
+x_end <- sapply(strsplit(sapply(strsplit(x_names, "__"), "[[", 3), ".tre"), "[[", 1)
+
+
+# write tree info
+write.table(cbind(x_chrom, x_start, x_end), file="camphybrid_twisst_50kbp_tree_info.txt", sep="\t", quote=F, row.names=F, col.names=F)
+
+# trees into one file
+tree_list <- list()
+for(a in 1:length(x_files)) {
+	tree_list[[a]] <- scan(x_files[a], what="character")
+}
+
+tree_list <- unlist(tree_list)
+write(tree_list, file="camphybrid_twisst.trees", ncolumns=1)
+
+
+
+
+
+
+
+
+
+
